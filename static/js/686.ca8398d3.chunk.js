f9871e4757c007b7af3389f92970d549ce7d4b44
MarinaTripetska <80358290+MarinaTripetska@users.noreply.github.com>
2022-05-15 11:23:01 +0000

    Deploying to gh-pages from @ MarinaTripetska/goit-react-hw-08-phonebook@15ad027e4d3a9024b142fae9ea42e6c9b8b31909 🚀

--- static/js/686.ca8398d3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[686,680],{680:function(n,e,t){t.r(e);var r=t(184);e.default=function(){return(0,r.jsx)("main",{children:(0,r.jsx)("div",{children:"Main page"})})}},1686:function(n,e,t){t.r(e),t.d(e,{default:function(){return r.default}});var r=t(680)}}]);
-//# sourceMappingURL=686.ca8398d3.chunk.js.map
