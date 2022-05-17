550aa69d8a6df6e204e25d219ec7c5371e505940
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-05-18 01:17:39 +0200

    fix: delete theme and themeProvider

--- src/index.js
+++ src/index.js
@@ -7,23 +7,19 @@ import { Toaster } from 'react-hot-toast';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import { theme } from 'components/ColorTheme';
-import { ThemeProvider } from '@mui/material';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <App />
-            <Toaster />
-          </PersistGate>
-        </Provider>
-      </ThemeProvider>
+      <GlobalStyle />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+          <Toaster />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
